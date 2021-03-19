2d55b253dafafdf939963c6925103105c2b5e58b
TomGarfild <safronuyk.allex73@gmail.com>
2021-03-19 20:46:34 +0200

    Add documentation and producesing codes.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,29 +1,51 @@
 ﻿using System;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VirtualSports.BE.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ApiController]
     public class AuthController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public AuthController()
         {
 
         }
 
-        [HttpPost()]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("register")]
+        [ProducesResponseType((int)HttpStatusCode.Conflict)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Register()
         {
             throw new NotImplementedException();
         }
 
-        [HttpPost()]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("login")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public IActionResult Login()
         {
             throw new NotImplementedException();
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpPut()]
         public IActionResult Logout()
         {
