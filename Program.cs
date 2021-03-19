fde780e2287ecd25f6b81176a77ce7d423975ed4
TomGarfild <safronuyk.allex73@gmail.com>
2021-03-19 19:41:07 +0200

    Add documentation

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace VirtualSports.BE
 {
+#pragma warning disable 1591
     public class Program
+
     {
         public static void Main(string[] args)
         {
@@ -23,4 +19,5 @@ namespace VirtualSports.BE
                     webBuilder.UseStartup<Startup>();
                 });
     }
+#pragma warning restore 1591
 }
--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.OpenApi.Models;
 
 namespace VirtualSports.BE
 {
+
+#pragma warning disable 1591
     public class Startup
+
     {
         public Startup(IConfiguration configuration)
         {
@@ -23,13 +21,12 @@ namespace VirtualSports.BE
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DI Demo App API", Version = "v1" });
-                var filePath = Path.Combine(AppContext.BaseDirectory, "DepsWebApp.xml");
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "VirtualSports", Version = "v1" });
+                var filePath = Path.Combine(AppContext.BaseDirectory, "VirtualSports.BE.xml");
                 if (File.Exists(filePath))
                 {
                     c.IncludeXmlComments(filePath);
@@ -64,7 +61,6 @@ namespace VirtualSports.BE
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -87,4 +83,5 @@ namespace VirtualSports.BE
             });
         }
     }
+#pragma warning restore 1591
 }
