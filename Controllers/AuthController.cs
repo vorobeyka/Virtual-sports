943ef03a3db2a41c112798b183e707f293120a6e
TomGarfild <safronuyk.allex73@gmail.com>
2021-03-19 19:28:41 +0200

    Add auth controller and swagger

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualSports.BE.Controllers
+{
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpPost()]
+        public IActionResult Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost()]
+        public IActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPut()]
+        public IActionResult Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace VirtualSports.BE
 {
@@ -24,6 +26,41 @@ namespace VirtualSports.BE
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DI Demo App API", Version = "v1" });
+                var filePath = Path.Combine(AppContext.BaseDirectory, "DepsWebApp.xml");
+                if (File.Exists(filePath))
+                {
+                    c.IncludeXmlComments(filePath);
+                }
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "Session",
+                                    Type = ReferenceType.SecurityScheme
+                                },
+                            },
+                            new string[0]
+                        }
+                    });
+                c.AddSecurityDefinition(
+                    "Session",
+                    new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.ApiKey,
+                        In = ParameterLocation.Header,
+                        Scheme = "Session",
+                        Name = "Authorization",
+                        Description = "SessionId",
+                        BearerFormat = "SessionId"
+                    });
+            });
             services.AddControllers();
         }
 
@@ -32,11 +69,16 @@ namespace VirtualSports.BE
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
