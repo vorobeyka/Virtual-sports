672e1b97cb760830457c42ac17a41af6d424df58
TomGarfild <safronuyk.allex73@gmail.com>
2021-03-22 23:24:03 +0200

    Add session

--- Models/DatabaseModels/Session.cs
+++ Models/DatabaseModels/Session.cs
@@ -2,11 +2,26 @@
 
 namespace VirtualSports.BE.Models.DatabaseModels
 {
+    /// <summary>
+    /// Model for table sessions.
+    /// </summary>
+    [Table("Sessions")]
     public class Session
     {
+        /// <summary>
+        /// Unique id.
+        /// </summary>
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Jwt token.
+        /// </summary>
         public string Token { get; set; }
+
+        /// <summary>
+        /// True if jwt token is valid.
+        /// </summary>
         public bool IsValid { get; set; }
     }
 }
