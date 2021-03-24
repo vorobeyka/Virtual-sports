9d79fc051d013e154013277af71a98c8b6e2a7fc
vorobeyka <45212751+vorobeyka@users.noreply.github.com>
2021-03-24 12:24:03 +0200

    Open swagger for all

--- VirtualSports.Web/Startup.cs
+++ VirtualSports.Web/Startup.cs
@@ -111,14 +111,14 @@ namespace VirtualSports.Web
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Virtual Sports API V1");
                 });
-            }
+            //}
 
             app.UseRouting();
 
